ab9de679e10ed6dc60b36dc7c0d103843605d059
kyujin <g.mikekim@gmail.com>
2017-03-10 16:32:59 -0500

    first commit

---
+++ app/src/main/java/com/example/mkim/webviewtest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mkim.webviewtest;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        // Configure related browser settings
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        myWebView.setWebViewClient(new MyBrowser());
+        // Load the initial URL
+        myWebView.loadUrl("https://www.google.com");
+    }
+
+    // Manages the behavior when URLs are loaded
+    private class MyBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+    }
+}
